b3b7de669d032e65a6a451ca24140113dffaebd1
Ryo Richie <ryorichie@gmail.com>
2025-01-09 20:51:30 +0700

    fix minor

--- src/routes/specialization/+layout.svelte
+++ src/routes/specialization/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import '../app.css';
 	import Rightsidebar from '../../components/rightsidebar.svelte';
 	import Background from '../../components/background.svelte';
 	let { children } = $props();
